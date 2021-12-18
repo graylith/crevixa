8a8b098a7b3a5cf0dd98e20f5c8841b628cf13b7
graylith <44845049+graylith@users.noreply.github.com>
2021-12-18 21:34:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+preLoader()
+
+function preLoader(){
+    var tl = gsap.timeline();
+    tl.to(".preLoader-front", {
+        duration: 1.2,
+        height:"100%",
+        top: "-100%",
+        ease: "Expo.easeInOut",
+    });
+
+    tl.to(".preLoader-front", {
+        duration: 1,
+        width: "100%",
+        top: "-200%",
+        ease: "Expo.easeInOut",
+        delay: 0.3,
+    });
+    tl.to(".preLoader-back", {
+        duration: 1,
+        width: "100%",
+        top: "-200%",
+        ease: "Expo.easeInOut",
+        delay: -1
+    });
+    
+    
+}
+
+var box = document.querySelector(".box");
+
+box.addEventListener("click", function(e){
+    console.log("hello")
+    
+    window.location.href = 'https://code.tutsplus.com';
+    
+})
